944b9220969866666b9130a91a72a0ad9739a498
theesean <36942778+theesean@users.noreply.github.com>
2018-02-28 21:18:06 -0600

    Add files via upload

---
+++ Code1.ino
@@ -0,0 +1,33 @@
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  13;      // the number of the LED pin
+
+// variables will change:
+volatile int buttonState = 0;         // variable for reading the pushbutton status
+volatile unsigned int lasttime = 0, currenttime = 0;
+unsigned int timechange = 0;
+
+void setup() {
+  // initialize the LED pin as an output:
+  pinMode(ledPin, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+  // Attach an interrupt to the ISR vector
+  attachInterrupt(0, pin_ISR, RISING);
+   Serial.begin(9600);
+}
+
+void loop() {
+
+timechange = currenttime - lasttime;
+Serial.println(timechange);
+
+
+}
+
+void pin_ISR() {
+  buttonState = digitalRead(buttonPin);
+  digitalWrite(ledPin, buttonState);
+  lasttime = currenttime;
+  currenttime = millis();
+  
+}
