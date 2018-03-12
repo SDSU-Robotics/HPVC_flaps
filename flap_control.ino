3abd45ec7a464084bd2c5b9b7d2095c4c719343b
Joel Quanbeck <jqquanbeck@gmail.com>
2018-03-12 09:39:38 -0500

    Cleaned up code; eliminated unnecessary functionality

--- flap_control.ino
+++ flap_control.ino
@@ -1,15 +1,12 @@
 const int buttonPin = 2;     // the number of the pushbutton pin
-const int ledPin =  13;      // the number of the LED pin
 
 // variables will change:
-volatile int buttonState = 0;         // variable for reading the pushbutton status
 volatile unsigned int lasttime = 0, currenttime = 0;
 unsigned int timechange = 0;
 volatile bool updated = false;
+unsigned int position = 0; //position of the flap
 
 void setup() {
-  // initialize the LED pin as an output:
-  pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
   // Attach an interrupt to the ISR vector
@@ -21,14 +18,13 @@ void loop() {
   if (updated) // only update if there is a new value
   {
     timechange = currenttime - lasttime;
-    Serial.println(timechange);
     updated = false;
+    position = timechange*timechange;
+    Serial.println(position);
   }
 }
 
 void pin_ISR() {
-  buttonState = digitalRead(buttonPin);
-  digitalWrite(ledPin, buttonState);
   
   lasttime = currenttime;
   currenttime = millis();
