401346c0f2529d947c8cfbdbf3d5ab618874b4f8
theesean <36942778+theesean@users.noreply.github.com>
2018-03-12 17:18:44 -0500

    fixed interrupt, added position

--- flap_control.ino
+++ flap_control.ino
@@ -1,31 +1,41 @@
-const int buttonPin = 2;     // the number of the pushbutton pin
+const int hallPin = 2;     // the number of the hall effect pin
+const int breakpin = 3;
 
-// variables will change:
 volatile unsigned int lasttime = 0, currenttime = 0;
 unsigned int timechange = 0;
 volatile bool updated = false;
 unsigned int position = 0; //position of the flap
+double speed;
+
+
+const double K = 1.0; // coeffecient for speed^2
 
 void setup() {
   // initialize the pushbutton pin as an input:
-  pinMode(buttonPin, INPUT);
+  pinMode(hallPin, INPUT);
   // Attach an interrupt to the ISR vector
-  attachInterrupt(0, pin_ISR, RISING);
-  Serial.begin(9600);
+  attachInterrupt(digitalPinToInterrupt (2), pin_ISR, RISING);
+  Serial.begin(115200);
 }
 
 void loop() {
-  if (updated) // only update if there is a new value
+
+  if (digitalRead(breakpin) == HIGH)
+  {
+    position = 0;
+  }
+  else if (updated) // only update if there is a new value
   {
     timechange = currenttime - lasttime;
     updated = false;
-    position = timechange*timechange;
-    Serial.println(position);
+    speed = 1000.0 / timechange;
+    position = K * speed * speed;
   }
+  Serial.println(position);
 }
 
 void pin_ISR() {
-  
+
   lasttime = currenttime;
   currenttime = millis();
   
