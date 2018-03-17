6b8bf7547abfbe033e8e5a747299427ae1f98177
Joel Quanbeck <jqquanbeck@gmail.com>
2018-03-17 12:36:29 -0500

    Added activate switch, MIN_POS, EXP constant

--- flap_control.ino
+++ flap_control.ino
@@ -1,58 +1,75 @@
-const int DIR = 8;
-const int PUL = 13;
+// CONSTANTS
+const int MIN_POS = 250;     // minimum flap position
+const int MAX_POS = 500;     // maximum flap position
+const double EXP = 2;        // exponent
+const double K = (MAX_POS - MIN_POS) / pow(2.895, EXP); // coeffecient for speed^EXP
 
-int currentPos = 0;
-int newPos = 0;
+// INTERNAL USE BELOW
 
-const int MAX_POS = 200;
+const int hallPin = 2;       // hall effect pin
+const int breakpin = 3;      // break button pin
+const int DIR = 4;           // DIR stepper driver pin
+const int PUL = 5;           // PUL stepper driver pin
+const int activate_pin = 6;  // switch to enable/disable speed dependent flap control
 
-const int hallPin = 2;     // the number of the hall effect pin
-const int breakpin = 3;
+int currentPos = 0;          // current position. Updated by stepUp and stepDown
+int newPos = 0;              // new position move to
 
 volatile unsigned int lasttime = 0, currenttime = 0;
 unsigned int duration = 0;
 volatile bool updated = false;
-double speed = 0;
+double speed = 0;            // speed in rev/sec
 
-const double K = 4.0; // coeffecient for speed^2
-
-const int numReadings = 10;
-int readings[numReadings];      // the readings from the analog input
-int readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-int average = 0;                // the average
+const int numReadings = 10;               // number of readings to average
+int readings[numReadings] = { MIN_POS };  // the readings from the analog input
+int readIndex = 0;                        // the index of the current reading
+int total = MIN_POS * numReadings;        // the running total
+int average = MIN_POS;                    // the average
 
 
 void setup()
 {
+  // stepper driver outputs
   pinMode(DIR, OUTPUT);
   pinMode(PUL, OUTPUT);
-  
-  // initialize the pushbutton pin as an input:
+
+  // sensors and switches
   pinMode(hallPin, INPUT_PULLUP);
+  pinMode(breakpin, INPUT_PULLUP);
+  pinMode(activate_pin, INPUT_PULLUP);
   
   // Attach an interrupt to the ISR vector
   attachInterrupt(digitalPinToInterrupt (2), pin_ISR, RISING);
   Serial.begin(115200);
+
+  // initialize location to MIN_POS
+  setPosition(MIN_POS);
 }
 
 void loop()
 {
-  if (digitalRead(breakpin) == HIGH)
+  if (!digitalRead(breakpin) == HIGH)
   {
     setPosition(0);
   }
+  else if (!digitalRead(activate_pin))
+  {
+    setPosition(MAX_POS);
+  }
   else if (updated) // only update if there is a new value
   {
+    // calculated speed
     duration = currenttime - lasttime;
     speed = 1000.0 / duration;
-    newPos = K * speed * speed;
+
+    // calculate new position
+    newPos = K * pow(speed, EXP) + MIN_POS;
 
     // constrain position
     if (newPos > MAX_POS)
       newPos = MAX_POS;
-    else if (newPos < 0)
-      newPos = 0;
+    else if (newPos < MIN_POS)
+      newPos = MIN_POS;
 
     // subtract the last reading:
     total = total - readings[readIndex];
@@ -71,10 +88,14 @@ void loop()
 
     // calculate the average:
     average = total / numReadings;
-    
-    Serial.print("newPos: ");
-    Serial.println(average);
 
+    // constrain position
+    if (average > MAX_POS)
+      average = MAX_POS;
+    else if (average < MIN_POS)
+      average = MIN_POS;
+
+    // move flaps
     setPosition(average);
 
     updated = false;
@@ -83,8 +104,10 @@ void loop()
 
 void stepUp()
 {
+  // set direction
   digitalWrite(DIR, HIGH);
-  
+
+  // pulse driver
   digitalWrite(PUL, LOW);
   digitalWrite(PUL, HIGH);
   digitalWrite(PUL, LOW);
@@ -95,8 +118,10 @@ void stepUp()
 
 void stepDown()
 {
+  // set direction
   digitalWrite(DIR, LOW);
-  
+
+  // pulse driver
   digitalWrite(PUL, LOW);
   digitalWrite(PUL, HIGH);
   digitalWrite(PUL, LOW);
@@ -122,13 +147,6 @@ void setPosition(int pos)
 }
 
 void pin_ISR() {
-  /*int tempTime = millis();
-  if (tempTime - lasttime > 30) // valid data
-  {
-    lasttime = currenttime;
-    currenttime = tempTime;
-    updated = true;
-  }*/
   lasttime = currenttime;
   currenttime = millis();
   updated = true;
